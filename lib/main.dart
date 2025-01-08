e6f867f3adc09abba08ea547b03052f1ac432f14
skheiar <skheiar@E201-17>
2025-01-08 09:00:32 +0100

    qr code

--- lib/main.dart
+++ lib/main.dart
@@ -1,126 +1,53 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter_hackathon/barcode_scanner_simple.dart';
+import 'package:flutter_hackathon/mobile_scanner_overlay.dart';
 
 
-
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    /*SystemChrome.setSystemUIOverlayStyle(
-      const SystemUiOverlayStyle(
-        statusBarColor: Colors.white,
-        statusBarIconBrightness: Brightness.dark,
-        systemNavigationBarColor: Colors.white,
-        systemNavigationBarIconBrightness: Brightness.dark,
-      ),
-    );*/
-    return MaterialApp(
-      title: 'QR Code Scanner App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+void main() {
+  runApp(
+    const MaterialApp(
+      title: 'Mobile Scanner test',
+      home: MyHome(),
       debugShowCheckedModeBanner: false,
-      home: const MyHomePage(title: 'QR Scanner Home'),
-    );
-  }
+    ),
+  );
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class MyHome extends StatelessWidget {
+  const MyHome({super.key});
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
+  Widget _buildItem(BuildContext context, String label, Widget page) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
         child: ElevatedButton(
           onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const QRCodeScannerPage()),
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => page,
+              ),
             );
           },
-          child: const Text('Scanner un QR Code'),
+          child: Text(label),
         ),
       ),
     );
   }
-}
-
-class QRCodeScannerPage extends StatefulWidget {
-  const QRCodeScannerPage({super.key});
 
   @override
-  State<QRCodeScannerPage> createState() => _QRCodeScannerPageState();
-}
-
-class _QRCodeScannerPageState extends State<QRCodeScannerPage> {
-  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
-  Barcode? result;
-  QRViewController? controller;
-
-  void _onQRViewCreated(QRViewController controller) {
-    this.controller = controller;
-    controller.scannedDataStream.listen((scanData) {
-      setState(() {
-        result = scanData;
-      });
-      showDialog(
-
-        // ignore: use_build_context_synchronously
-        context: context,
-        builder: (context) => AlertDialog(
-          title: const Text('QR Code Scanné'),
-          content: Text('Les données du QR Code : ${scanData.code}'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-                controller.resumeCamera();
-              },
-              child: const Text('Fermer'),
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Mobile Scanner test')),
+      body: Center(
+        child: ListView(
+          children: [
+            _buildItem(
+              context,
+              'MobileScanner ',
+              const BarcodeScannerSimple(),
             ),
           ],
         ),
-      );
-    });
-  }
-
-  @override
-  void dispose() {
-    controller?.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Scanner QR Code'),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: QRView(
-              key: qrKey,
-              onQRViewCreated: _onQRViewCreated,
-            ),
-          ),
-        ],
       ),
     );
   }
